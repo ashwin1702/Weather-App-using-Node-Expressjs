871e9ed7799bb7a28be182743c87743cff7d6e96
ashwin1702 <joshiashwin9754@gmail.com>
2022-08-10 10:03:12 +0530

    nodejs project first commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const { response } = require('express');
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app= express();
+const port = process.env.PORT || 3000 ;//phle 3000 prr chlega agr wha host nhi hua toh kisi or port prr hoga jo hum denge
+const staticPath=path.join(__dirname, "../public");
+const  template_path=path.join(__dirname, "../templates/views");
+const partials_path= path.join(__dirname,"../templates/partials")
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path)
+app.use(express.static(staticPath));
+app.get("", (req,res)=>{
+    res.render("index")
+})
+
+app.get("/about", (req,res)=>{
+    res.render("about")
+})
+
+app.get("/weather", (req,res)=>{
+    res.render("weather")
+})
+
+app.get("*", (req,res)=>{
+    res.send("error 404, Page not found")
+})
+app.listen(port , ()=>{
+    console.log(`server is listening at ${port}`);
+})
